a1f50435727c8c39653e34f1ce7a8083d9bed70b
eatasandwich1 <eatasandwich1@users.noreply.github.com>
2018-07-17 14:03:06 -0500

    Comments
    Added comments

--- CountSpaces/countspaces.c
+++ CountSpaces/countspaces.c
@@ -3,21 +3,22 @@
 // This program counts the amount of spaces in count.txt.
 int main() {
 
-    FILE *textfile = fopen("count.txt", "r");
+    FILE *textfile = fopen("count.txt", "r"); // Open file for reading
     int c = 32; // Space character
-    int i = 0;
+    int i = 0; // Space counter
 
     while((c=fgetc(textfile))){
-        if(c == EOF) break;
-        if(c == 32) i++;
+        if(c == EOF) break; // Stop when we reach the end of file
+        if(c == 32) i++; // Count the amount of spaces
     }
 
     if (i > 0){
         printf("Number of spaces: %d \n", i);
+		// If there are any spaces at all
     }
     else{
         printf("No spaces found");
     }
-    fclose(textfile);
+    fclose(textfile); // Close file
     return 0;
 }
